e26513291039f86e45f135c72fdc1fba4f343d22
Kavit Shah <kavit.shah8@gmail.com>
2013-07-21 13:58:16 -0700

    SDN(Georgia Tech) : Assignment 4 , Creating fully functionla layer 2 firewall

---
+++ pox/misc/firewall.py
@@ -0,0 +1,50 @@
+'''
+Coursera:
+- Software Defined Networking (SDN) course
+-- Module 4 Programming Assignment
+
+Professor: Nick Feamster
+Teaching Assistant: Muhammad Shahbaz
+'''
+
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+from pox.lib.revent import *
+from pox.lib.util import dpidToStr
+from pox.lib.addresses import EthAddr
+from collections import namedtuple
+import os
+''' Add your imports here ... '''
+from csv import DictReader
+
+
+log = core.getLogger()
+policyFile = "%s/pox/pox/misc/firewall-policies.csv" % os.environ[ 'HOME' ]  
+
+''' Add your global variables here ... '''
+
+class Firewall (EventMixin):
+
+    def __init__ (self):
+        self.listenTo(core.openflow)
+        log.debug("Enabling Firewall Module")
+
+    def _handle_ConnectionUp (self, event):    
+        ''' Add your logic here ... '''
+        with open(policyFile,'r') as csvfile:
+            dictreader = DictReader(csvfile)
+            for rule in dictreader:
+                # print rule
+                msg = of.ofp_flow_mod()
+                msg.priority = 65535
+                msg.match.dl_src = EthAddr(rule['mac_0'])
+                msg.match.dl_dst = EthAddr(rule['mac_1'])
+                event.connection.send(msg)
+    
+        log.debug("Firewall rules installed on %s", dpidToStr(event.dpid))
+
+def launch ():
+    '''
+    Starting the Firewall module
+    '''
+    core.registerNew(Firewall)
